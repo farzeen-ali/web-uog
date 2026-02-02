9ac8391124a3bfbae916af6399abcae6da87aee9
farzeen-ali <farzeenaliarif786@gmail.com>
2026-02-02 11:12:15 +0500

    JS Basics

---
+++ script.js
@@ -0,0 +1,33 @@
+// Variables
+// var, let, const
+// var age = 23;
+// var age = 3; // redeclare allow
+
+// console.log(age);
+
+// let
+// let firstName = "Ali";
+// let firstName = "Farzeen"; redeclaration not allowed
+// firstName = "Farzeen"
+// console.log(firstName)
+
+// const
+// reinitialization & redeclaration not allowed
+// const number = 20;
+// number = 30;
+// console.log(number)
+
+// arrays
+// let arr = ["Maths", true, 23, 'c'];
+// console.log(arr)
+
+// objects
+// let obj = {name: "Ali", age: 23, isOnline: true};
+
+// console.log(obj.age)
+
+// function
+function changeText(){
+    let heading = document.getElementById('heading');
+    heading.innerHTML = "Farzeen Ali"
+}
